9307777f6027f2d6e5fcd072f23022bae6e74826
Abhishek Chaturvedi <chaturvedivaibhav426@gmail.com>
2026-08-07 07:24:18 +0530

    Update friends-pairing-problem.cpp
    Optimized....
    the Problem is related To Modular arithmetic and has recursive approach.. So Applying DP is Must.

--- Difficulty: Medium/Friends Pairing Problem/friends-pairing-problem.cpp
+++ Difficulty: Medium/Friends Pairing Problem/friends-pairing-problem.cpp
@@ -1,12 +1,20 @@
 class Solution {
     private:
-    int count_freinds_pair(int n){
+    int count_freinds_pair(int n, vector<int> &dp){
         if(n<=1)return 1;
-        return count_freinds_pair(n-1) + (n-1)* count_freinds_pair(n-2);
+        if(dp[n]!=-1)return dp[n];
+        return dp[n]= count_freinds_pair(n-1, dp) + (n-1)* count_freinds_pair(n-2, dp);
     }
   public:
     int countFriendsPairings(int n) {
         // code here
-        return count_freinds_pair(n);
+        vector<int> dp(n+1, 0);
+        // return count_freinds_pair(n, dp);
+        dp[0]=1;
+        dp[1]=1;
+        for(int i=2; i<=n; i++){
+            dp[i]= dp[i-1] + (i-1) * dp[i-2];
+        }
+        return dp[n];
     }
 };
