383d280c29c0c3762bafd884ee13af1e70b29be2
Abhishek Chaturvedi <chaturvedivaibhav426@gmail.com>
2026-08-11 21:43:37 +0530

    Add binary search method for nth root calculation
    Implemented a second approach for finding the nth root using binary search.

--- Difficulty: Medium/Find nth root of m/find-nth-root-of-m.cpp
+++ Difficulty: Medium/Find nth root of m/find-nth-root-of-m.cpp
@@ -1,3 +1,5 @@
+
+// APProach 1
 class Solution {
 	public:
 	int nthRoot(int n, int m) {
@@ -10,3 +12,30 @@ class Solution {
 			return - 1;
 	}
 };
+
+
+// Second approach
+
+class Solution {
+	public:
+	int nthRoot(int n, int m) {
+		// Code here
+		if (m == 0)return 0;
+		int l = 1;
+		int h = m;
+		while (l <= h) {
+			int mid = l + (h - l)/2;
+			if (pow(mid, n) == m) {
+				return mid;
+			}
+			else if (pow(mid, n) > m) {
+				h = mid - 1;
+			}
+			else {
+				l = mid + 1;
+			}
+		}
+		return - 1;
+	}
+};
+
