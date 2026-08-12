c6f330b298f334bba2006c05e45b1e101e9b476e
Abhishek Chaturvedi <chaturvedivaibhav426@gmail.com>
2026-08-13 00:42:13 +0530

    Add brute force solution for maze pathfinding
    Implemented a brute force method to find paths and maximum sum in a maze.

--- Difficulty: Hard/Adventure in a Maze/adventure-in-a-maze.cpp
+++ Difficulty: Hard/Adventure in a Maze/adventure-in-a-maze.cpp
@@ -1,3 +1,51 @@
+// Method 1 Bruete Force///
+
+class Solution {
+    private:
+    int find_ways_and_path(vector<vector<int>> &grid, int i, int j, int &maxi, int sum){
+        if(i<0)return 0;
+        if(i>=grid.size())return 0;
+        if(j<0)return 0;
+        if(j>=grid[i].size())return 0;
+        if(i==grid.size()-1 and j==grid[i].size()-1){
+            sum+=grid[i][j];
+            maxi= max(maxi, sum);
+            return 1;
+        }
+        int count=0;
+        //right move karo only
+        if(grid[i][j]==1){
+            count+= find_ways_and_path(grid, i, j+1, maxi, sum+grid[i][j]);
+            return count;
+        }
+        //down move karo
+        if(grid[i][j]==2){
+            count+= find_ways_and_path(grid, i+1, j, maxi,sum+grid[i][j]);
+            return count;
+        }
+        //2 options hain
+        if(grid[i][j]==3){
+            count+=find_ways_and_path(grid, i, j+1, maxi,sum+grid[i][j])+ find_ways_and_path(grid, i+1, j,maxi, sum+grid[i][j]);
+            return count;
+        }
+    }
+  public:
+    vector<int> findWays(vector<vector<int>>& grid) {
+        // code here
+        int maxi= 0;
+        int sum=0;
+        vector<vector<int>> dp(grid.size()+1, vector<int> (grid.size()+1, -1));
+        int length= find_ways_and_path(grid, 0, 0, maxi, sum);
+        vector<int> ans;
+        ans.push_back(length);
+        ans.push_back(maxi);
+        return ans;
+    }
+};
+
+
+// Method 2 Optimal and Expected......
+
 class Solution {
 	private:
 	const int mod= 1e9 + 7;
