f84e5c93374629b0bdea6e046e66c069dab0ca00
Abhishek Chaturvedi <chaturvedivaibhav426@gmail.com>
2026-08-15 13:10:06 +0530

    Add profession determination logic with optimal approach
    Implemented a solution to determine profession based on level and position in a binary tree structure. Added an optimal approach using bit manipulation to improve efficiency.

--- Difficulty: Medium/Finding Profession/finding-profession.cpp
+++ Difficulty: Medium/Finding Profession/finding-profession.cpp
@@ -1,3 +1,76 @@
+// Approach1
+class Node{
+    public:
+    char child;
+    Node* left;
+    Node* right;
+    Node(char child){
+       this->child= child;
+       left= nullptr;
+       right= nullptr;
+    }
+};
+class Solution {
+  public:
+    string profession(int level, int pos) {
+          // code here
+        queue<pair<Node*, pair<int, int>>> q;
+        Node* node= new Node('E');
+        q.push(make_pair(node, make_pair(1, 1)));
+        char ans;
+        while(!q.empty()){
+            int size= q.size();
+            int count=0;
+            bool flag= false;
+            while(size--){
+            pair<Node*, pair<int , int>> front= q.front();
+            q.pop();
+            Node* node= front.first;
+            int lev= front.second.first;
+            int posi= front.second.second;
+            if(lev==level and pos==posi){
+                ans= node->child;
+                flag= true;
+                break;
+            }
+            if(node->child=='E'){
+                count++;
+                Node* temp= new Node('E');
+                node->left= temp;
+                // q.push(make_pair(temp, make_pair(lev+1, count))));
+                q.push(make_pair(temp, make_pair(lev+1, count)));
+                
+                count++;
+                temp= new Node('D');
+                node->right= temp;
+                // q.push(make_pair(temp, make_pair(lev+1, make_pair(count))));
+                q.push(make_pair(temp, make_pair(lev+1, count)));
+                
+            }
+            else if(node->child=='D'){
+                 count++;
+                Node* temp= new Node('D');
+                node->left= temp;
+                // q.push(make_pair(temp, make_pair(lev+1, make_pair(count))));
+                q.push(make_pair(temp, make_pair(lev+1, count)));
+                
+                count++;
+                temp= new Node('E');
+                node->right= temp;
+                // q.push(make_pair(temp, make_pair(lev+1, make_pair(count))));
+                q.push(make_pair(temp, make_pair(lev+1, count)));
+            }
+            }
+            if(flag)break;
+        }
+        return ans=='D' ? "Doctor" : "Engineer";
+    }
+};
+
+
+
+// Approqach 2 Optimal
+
 class Solution {
   public:
     string profession(int level, int pos) {
