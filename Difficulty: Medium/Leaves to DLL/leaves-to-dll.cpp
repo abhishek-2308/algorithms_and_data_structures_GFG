24b10a0a3c549a40acffb176617d6d8c32922a31
Abhishek Chaturvedi <chaturvedivaibhav426@gmail.com>
2026-08-26 12:44:21 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Leaves to DLL/leaves-to-dll.cpp
@@ -0,0 +1,48 @@
+/* Binary Tree Node Structure
+class Node {
+	public:
+	int data;
+	Node *left;
+	Node *right;
+	
+	Node(int val) {
+		data = val;
+		left = right = nullptr;
+	}
+};
+*/
+class Solution {
+	private:
+	Node* remove_leve_nodes(Node* root, vector<Node*> &vec) {
+		if (!root) {
+			return nullptr;
+		}
+		if (!root->left and !root->right) {
+			vec.push_back(root);
+			return nullptr;
+		}
+		root->left = remove_leve_nodes(root->left, vec);
+		root->right = remove_leve_nodes(root->right, vec);
+		return root;
+	}
+	public:
+	Node* convertToDLL(Node* root) {
+		// code here
+		vector<Node*> vec;
+		Node* real_root = remove_leve_nodes(root, vec);
+		if (vec.empty()) {
+			return real_root;
+		}
+		
+		vec[0]->left = nullptr;
+		
+		for (int i = 1; i < vec.size(); i++) {
+			vec[i]->left = vec[i - 1];
+			vec[i - 1]->right = vec[i];
+		}
+		
+		vec.back()->right = nullptr;
+		
+		return vec[0];
+	}
+};
